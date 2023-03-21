ee05f6e9c64d671a3eb51dc54271dd8ca0394590
entaku <entaku19890818@gmail.com>
2023-03-21 16:22:41 +0900

    add scrape

--- index.js
+++ index.js
@@ -11,12 +11,31 @@ const fetch = require('node-fetch');
 const puppeteer = require('puppeteer');
 
 exports.scrapeGoogle = async (req, res) => {
+
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.google.com');
-    const title = await page.title();
+
+    await page.goto('https://www.bbc.com/sport/football', { waitUntil: 'networkidle2' });
+
+    // ニュースの見出しと画像URLを取得する
+    const news = await page.$$eval('.gs-c-promo', newsList => {
+        return newsList.map(news => {
+            const headline = news.querySelector('.gs-c-promo-heading__title').textContent;
+            const img = news.querySelector('img[srcset]') ? news.querySelector('img[srcset]').srcset.split(' ')[0] : null;
+            const url = news.querySelector('a[href]').href;
+            const summary = news.querySelector('.gs-c-promo-summary') ? news.querySelector('.gs-c-promo-summary').textContent : null;
+
+            return { headline, img, url,summary };
+        });
+    });
+
+
+    console.log(news);
+
+    // ブラウザを閉じる
     await browser.close();
-    res.status(200).send(title);
+    res.status(200).send(news);
 };
 
 
