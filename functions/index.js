307e339b68848423a5395dc2a68d091b10b77aed
entaku <entaku19890818@gmail.com>
2023-03-21 17:12:59 +0900

    add

--- functions/index.js
+++ functions/index.js
@@ -13,57 +13,60 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
-
-exports.scrapeGoogle = async (req, res) => {
-
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto('https://www.bbc.com/sport/football', { waitUntil: 'networkidle2' });
-
-    // ニュースの見出しと画像URLを取得する
-    const news = await page.$$eval('.gs-c-promo', newsList => {
-        return newsList.map(news => {
-            const headline = news.querySelector('.gs-c-promo-heading__title').textContent;
-            const img = news.querySelector('img[srcset]') ? news.querySelector('img[srcset]').srcset.split(' ')[0] : null;
-            const url = news.querySelector('a[href]').href;
-            const summary = news.querySelector('.gs-c-promo-summary') ? news.querySelector('.gs-c-promo-summary').textContent : null;
-
-            return { headline, img, url,summary };
-        });
-    });
-
-
-    console.log(news);
-
-    // ブラウザを閉じる
-    await browser.close();
-    res.status(200).send(news);
-};
-
-
-
-
-
-
-exports.addNewData = functions.https.onRequest(async (req, res) => {
+exports.scrapeGoogle = functions.https.onRequest(async (req, res) => {
     try {
-        // Firestoreコレクションを参照
-        const collectionRef = admin.firestore().collection('podcasts');
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-        // データを作成
-        const newData = {
-            headline: 'Fantasy 606 podcast: Free hit fishing across the pond',
-            url: 'https://www.bbc.co.uk/sounds/play/p0f90zph',
-        };
+        await page.goto('https://www.bbc.com/sport/football', { waitUntil: 'networkidle2' });
 
-        // Firestoreにデータを登録
-        const docRef = await collectionRef.add(newData);
-        const data = await docRef.get();
-        res.send(`Data added with ID: ${docRef.id} -> ${JSON.stringify(data.data())}`);
+        // ニュースの見出しと画像URLを取得する
+        const news = await page.$$eval('.gs-c-promo', newsList => {
+            return newsList.map(news => {
+                const headline = news.querySelector('.gs-c-promo-heading__title').textContent;
+                const img = news.querySelector('img[srcset]') ? news.querySelector('img[srcset]').srcset.split(' ')[0] : null;
+                const url = news.querySelector('a[href]').href;
+                const summary = news.querySelector('.gs-c-promo-summary') ? news.querySelector('.gs-c-promo-summary').textContent : null;
+
+                return { headline, img, url, summary };
+            });
+        });
+
+        console.log(news);
+
+        // ブラウザを閉じる
+        await browser.close();
+
+        // Firestoreにデータを登録する
+        await addNewData(news);
+
+        res.status(200).send(news);
     } catch (error) {
-        console.log(error);
-        res.status(500).send('Error adding data');
+        console.error(error);
+        res.status(500).send('Error scraping data');
     }
 });
+
+async function addNewData(news) {
+    try {
+        // Firestoreコレクションを参照
+        const collectionRef = admin.firestore().collection('books');
+
+        // Firestoreにデータを登録
+        const promises = news.map(async (article) => {
+            const docRef = await collectionRef.add({
+                title: article.headline,
+                imageUrl: article.img,
+                url: article.url,
+                description: article.summary,
+            });
+            const data = await docRef.get();
+            console.log(`Data added with ID: ${docRef.id} -> ${JSON.stringify(data.data())}`);
+        });
+
+        await Promise.all(promises);
+    } catch (error) {
+        console.error(error);
+        throw new functions.https.HttpsError('unknown', 'Error adding data');
+    }
+}
